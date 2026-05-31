250dbc26bbcaf8b181f1c4ec2d64053801f0bfe2
Alekhyo Biswas <alekhyobiswas2012@outlook.com>
2026-05-31 22:33:30 +0530

    Refactor opcode definitions with improved comments
    Updated opcode definitions and added comments for clarity.

--- include/opcodes.h
+++ include/opcodes.h
@@ -1,8 +1,12 @@
 #ifndef OPCODES_H
 #define OPCODES_H
 
-// Map these hex values to your Trie branches (e.g., 0x4131 = 'A1')
-#define OP_MOVE 0x4131
-#define OP_SAY  0x5359
+// Mapping hex values to instruction branches
+// Trie-routing: 0x41 = 'A', 0x31 = '1' -> 'A1'
+#define OP_MOVE    0x4131 
+#define OP_SAY     0x5359
+#define OP_IF      0x4946
+#define OP_JMP     0x4A4D
+#define OP_HALT    0x484C
 
 #endif
