0d7c56d77c412574ba82a24354490d9a377d40fd
Alekhyo Biswas <alekhyobiswas2012@outlook.com>
2026-06-01 22:19:29 +0530

    Update opcodes.h to define only OP_DISPLAY
    Removed old opcode definitions and added display opcode.

--- include/opcodes.h
+++ include/opcodes.h
@@ -1,12 +1,7 @@
 #ifndef OPCODES_H
 #define OPCODES_H
 
-// Mapping hex values to instruction branches
-// Trie-routing: 0x41 = 'A', 0x31 = '1' -> 'A1'
-#define OP_MOVE    0x4131 
-#define OP_SAY     0x5359
-#define OP_IF      0x4946
-#define OP_JMP     0x4A4D
-#define OP_HALT    0x484C
+// The only opcode: 'display' (0x4449 = 'DI' for display)
+#define OP_DISPLAY 0x4449
 
 #endif
